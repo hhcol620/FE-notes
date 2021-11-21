0f49ad7c782eb1211e6b1c7b5fe560ba1082d36d
hhcol <2838967868@qq.com>
2021-11-21 22:09:49 +0800

    '设置顶部导航栏'

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -16,15 +16,15 @@ module.exports = {
                 text: '分类', //默认显示
                 ariaLabel: '分类', //用于识别的label
                 items: [
-                    { text: '文章', link: '/pages/folder1/test1.md' },
+                    { text: '实践笔记', link: '/pages/folder1/test1.md' },
                     //点击标签会跳转至link的markdown文件生成的页面
-                    { text: '琐碎', link: '/pages/folder2/test4.md' }
+                    { text: '源码分析', link: '/pages/folder2/test4.md' }
                 ]
             },
-            { text: '功能演示', link: '/pages/folder1/test3.md' },
+            { text: '常用工具', link: '/pages/folder1/test3.md' },
 
             //格式三：跳转至外部网页，需http/https前缀
-            { text: 'Github', link: 'https://.com/dwanda' }
+            { text: 'Github', link: 'https://github.com/hhcol620' }
         ],
 
         //侧边导航栏：会根据当前的文件路径是否匹配侧边栏数据，自动显示/隐藏
